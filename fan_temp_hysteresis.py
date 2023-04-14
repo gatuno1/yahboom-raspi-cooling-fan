0485ed8643bb77673d3769987fe3c53cce8aa023
Antonio Sanhueza <antonio_sanhueza@hotmail.com>
2023-04-14 18:56:43 -0400

    Hysteresis - part 3

--- fan_temp_hysteresis.py
+++ fan_temp_hysteresis.py
@@ -7,11 +7,13 @@ from typing import Tuple
 DEV_ADDR = 0x0d
 FAN_REG = 0x08
 WRITE_RETRIES : int = 3
+
 temp_value : float
 hysteresis_temp : float = 10.0
-trigger_temp : float = 50.0
-fan_status = "OFF"
-last_fan_status = None
+trigger_temp : float = 52.0
+fan_status = ""
+last_fan_status : str = "FLOAT"
+
 values_dict = {"OFF": 0x00, "ON": 0x01}
 
 bus = smbus.SMBus(1)
@@ -19,10 +21,25 @@ pattern = re.compile(r'temp=(\d+\.\d+)')
 
 
 def set_fan(state: str):
-    retry = 0
-    value = values_dict[state]
-    if ++retry <= WRITE_RETRIES:
-        bus.write_byte_data(DEV_ADDR, FAN_REG, value)
+    attempt = 1
+    success = False
+    value : int = values_dict[state]
+    print("State: '{}', value: {}".format(state,value))
+    while not success and attempt <= WRITE_RETRIES:
+        try:
+            bus.write_byte_data(DEV_ADDR, FAN_REG, value)
+        except BaseException as e:
+            print("Failed attempt #{} to write.".format(attempt))
+            attempt += 1
+            if attempt > WRITE_RETRIES:
+                print("Giving up after {} attempts.".format(WRITE_RETRIES))
+                raise e
+            time.sleep(0.2)
+        else:
+            success = True
+            if attempt >= 1:
+                print("Write success on attempt #{}.".format(attempt))
+
 
 
 def read_temp() -> Tuple[float, str]:
@@ -40,22 +57,27 @@ def read_temp() -> Tuple[float, str]:
     return temp, temp_text;
 
 
-set_fan(fan_status)
-print("Init Fan: " + fan_status)
+# set_fan(fan_status)
+# print("Init Fan: " + fan_status)
 
 while True:
-    fan_status = ""
+    fan_status = None
     temp_value, temperature = read_temp()
 
     if temp_value >= trigger_temp:
-        last_fan_status = fan_status
         fan_status = "ON"
     elif temp_value <= trigger_temp - hysteresis_temp:
-        last_fan_status = fan_status
         fan_status = "OFF"
 
-    if fan_status != "" and fan_status != last_fan_status:
+    if fan_status != None:
+    #if fan_status != "" and fan_status != last_fan_status:
+        #last_fan_status = fan_status
         set_fan(fan_status)
+        print("Temp: {}°C, Fan: {}".format(temperature, fan_status))
+    else:
+        print("Temp: {}°C".format(temperature))
 
-    print("Temp: " + temperature + "°C" + ", Fan: " + fan_status)
     time.sleep(1.5)
+
+#OFF: i2cset -y 1 0x0d 0x08 0x00
+#ON:  i2cset -y 1 0x0d 0x08 0x01
