5fdbe99c6b158a0274c2143da8c5f8e21abf5e15
Antonio Sanhueza <antonio_sanhueza@hotmail.com>
2023-04-14 10:01:33 -0400

    Hysteresis  part 2

--- fan_temp_hysteresis.py
+++ fan_temp_hysteresis.py
@@ -2,11 +2,12 @@ import smbus
 import re
 import time
 import subprocess as sp
+from typing import Tuple
 
 DEV_ADDR = 0x0d
 FAN_REG = 0x08
 WRITE_RETRIES : int = 3
-temperature : float
+temp_value : float
 hysteresis_temp : float = 10.0
 trigger_temp : float = 50.0
 fan_status = "OFF"
@@ -24,7 +25,7 @@ def set_fan(state: str):
         bus.write_byte_data(DEV_ADDR, FAN_REG, value)
 
 
-def read_temp():
+def read_temp() -> Tuple[float, str]:
     proc = sp.run(['vcgencmd', 'measure_temp'], stdout=sp.PIPE, stderr=sp.STDOUT, universal_newlines=True)
     if proc.returncode != 0:
         print('Error: temperature reading failed.')
@@ -36,25 +37,25 @@ def read_temp():
         print('Error: temperature reading failed.')
         exit(1)
     temp = float(temp_text)
-    return temp, temp_text
+    return temp, temp_text;
 
 
 set_fan(fan_status)
 print("Init Fan: " + fan_status)
 
 while True:
-    fan_status = None
-    temperature, CPU_TEMP = read_temp(pattern)
+    fan_status = ""
+    temp_value, temperature = read_temp()
 
-    if temperature >= trigger_temp:
+    if temp_value >= trigger_temp:
         last_fan_status = fan_status
         fan_status = "ON"
-    elif temperature <= trigger_temp - hysteresis_temp:
+    elif temp_value <= trigger_temp - hysteresis_temp:
         last_fan_status = fan_status
         fan_status = "OFF"
 
-    if fan_status is not None and fan_status != last_fan_status:
+    if fan_status != "" and fan_status != last_fan_status:
         set_fan(fan_status)
 
-    print("Temp: " + CPU_TEMP + "°C" + ", Fan: " + fan_status)
+    print("Temp: " + temperature + "°C" + ", Fan: " + fan_status)
     time.sleep(1.5)
