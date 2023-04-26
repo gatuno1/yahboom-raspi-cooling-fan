d858200a6123c07b692786dc9722db99f048fe19
Antonio Sanhueza <antonio_sanhueza@hotmail.com>
2023-04-25 22:38:23 -0400

    Better formatting

--- fan_temp_hysteresis.py
+++ fan_temp_hysteresis.py
@@ -10,30 +10,31 @@ from systemd.journal import JournalHandler
 
 DEVICE_ADDR = 0x0d
 """i2c device address used by yahboom RBG fan hat."""
-FAN_REGISTER_ADDR = 0x08
+FAN_SPEED_REG = 0x08
 """i2c register address to regulate fan speed."""
 MODULE_NAME = "yahboom-fan-ctrl"
 """Module name used for configuration file and log file."""
 
 # Configuration global variables
-bus_number : int = 1
+bus_number: int = 1  #raspberry pi with 256MB uses bus_number = 0
 """i2c bus number."""
-log_file : str = "./" + MODULE_NAME + ".log"
+log_file: str = "./" + MODULE_NAME + ".log"
 """Log file path."""
-verbose : int = 1
+verbose: int = 1
 """Verbosity level of messages."""
 
-hysteresis_temp : float = 10.0
+hysteresis_temp: float = 10.0
 """Temperature hysteresis to turn off fan, in Celsius."""
-trigger_temp : float = 55.0
+trigger_temp: float = 55.0
 """Temperature at which fan is turned on in Celsius."""
 max_attempts = 3
 """Maximum number of attempts to write to i2c device."""
 sleep_seconds = 2.0
 """Time to sleep between temperature checks, in seconds."""
 
-# Reference for enums: https://docs.python.org/3/howto/enum.html
+
 class FanActions(Enum):
+    # Reference for enums: https://docs.python.org/3/howto/enum.html
     """Possible actions for fan."""
     NONE = 0x00
     """No action to be taken."""
@@ -42,17 +43,43 @@ class FanActions(Enum):
     ON = 0x02
     """Turn fan on."""
 
+
 # Global variables
-temperature : float
+temperature: float
 """Current CPU temperature in Celsius."""
-fan_action : FanActions
+fan_action: FanActions
 """Action to be taken by fan."""
-logger : logging.Logger
+logger: logging.Logger
 """Logger object to write to log file and stdout."""
 
-# Reference to get name: https://stackoverflow.com/a/35996948/17892898
-# Note: Method signal.Signals(signum) requires python 3.5 or higher.
+
+def init_communication(logger: logging.Logger):
+    """Initialize communication with i2c device, also writing to log.
+       If communication fails, exit with error code 2.
+    """
+    logger.info("Starting {} log.".format(MODULE_NAME))
+
+    try:
+        with smbus2.SMBus(bus_number) as i2c:
+            i2c.enable_pec(True)  # Enable "Packet Error Checking"
+            # Stop fan
+            i2c.write_byte_data(DEVICE_ADDR, FAN_SPEED_REG, 0x00)
+    except Exception as e:
+        msg = "Cannot open i2c device at bus {}, address '{}'! Aborting.\n".format(
+            bus_number, hex(DEVICE_ADDR))
+        logger.critical(msg, exc_info=True)
+        exit(2)
+    else:
+        logger.info("Connected successfully to i2c device at bus {}, address '{}'.".format(
+            bus_number, hex(DEVICE_ADDR)))
+
+    logger.info("Initial Temp: {:.2f}°C, trigger temp: >={:.2f}°C, hys. temp: {:.2f}°C ".format(
+        get_cpu_temp(), trigger_temp, -hysteresis_temp))
+
+
 def signal_name(signum: int) -> str:
+    # Reference to get name: https://stackoverflow.com/a/35996948/17892898
+    # Note: Method signal.Signals(signum) requires python 3.5 or higher.
     """Get signal name from signal value.
 
     Args:
@@ -74,12 +101,13 @@ def signal_name(signum: int) -> str:
 
 
 def tidyup(signal_num: int, frame):
-    """Write to log, turn off fan and exit program.
+    """Exit the program, after writing to log and turning off fan.
 
     Args:
         signal_num (int): signal value
     """
-    logger.info("Caught terminate signal '{}'. Turn fan off.\n".format(signal_name(signal_num)))
+    logger.info("Caught terminate signal '{}'. Turn fan off.\n".format(
+        signal_name(signal_num)))
     set_fan(FanActions.OFF)
     exit(0)
 
@@ -101,11 +129,13 @@ def get_cpu_temp() -> float:
         logger.critical(msg, exc_info=True)
         exit(3)
     except PermissionError:
-        msg = "Error: Permission denied to access temperature file '{}'.".format(CPU_TEMP_FILE)
+        msg = "Error: Permission denied to access temperature file '{}'.".format(
+            CPU_TEMP_FILE)
         logger.critical(msg, exc_info=True)
         exit(3)
     except:
-        msg = "Error: Unknown error reading temperature file '{}'.".format(CPU_TEMP_FILE)
+        msg = "Error: Unknown error reading temperature file '{}'.".format(
+            CPU_TEMP_FILE)
         logger.critical(msg, exc_info=True)
         exit(3)
     return temp
@@ -119,19 +149,22 @@ def set_fan(action: FanActions):
     """
     success = False
     attempt = 1
+    # 0x1 = 100% fan speed, 0x0 = 0% fan speed
     value = 0x01 if action == FanActions.ON else 0x00
     while not success and attempt <= max_attempts:
         try:
-            with smbus2.SMBus(bus_number) as i2c:
-                i2c.enable_pec(True)
-                i2c.write_byte_data(DEVICE_ADDR, FAN_REGISTER_ADDR, value)
+            with smbus2.SMBus(bus_number) as bus:
+                bus.enable_pec(True)
+                bus.write_byte_data(DEVICE_ADDR, FAN_SPEED_REG, value)
         except:
             if attempt <= max_attempts:
                 attempt += 1
                 if verbose >= 2:
-                    logger.exception("Write i2c error, attempt {}.".format(attempt), exc_info=True)
+                    logger.exception("Write i2c error, attempt {}.".format(
+                        attempt), exc_info=True)
             else:
-                msg = "Cannot write to i2c device after {} attempts.".format(attempt)
+                msg = "Cannot write to i2c device after {} attempts.".format(
+                    attempt)
                 logger.critical(msg, exc_info=True)
                 exit(2)
         else:
@@ -146,7 +179,9 @@ def read_config():
     config = configparser.ConfigParser()
 
     # Search for config file in /etc/yahboom-fan-ctrl/ and ./ directories
-    config_file_paths = ["/etc/{}/{}.conf".format(MODULE_NAME, MODULE_NAME), "./{}.conf".format(MODULE_NAME)]
+    config_file_paths = [
+        "/etc/{}/{}.conf".format(MODULE_NAME, MODULE_NAME),
+        "./{}.conf".format(MODULE_NAME)]
     for config_file_path in config_file_paths:
         if os.path.exists(config_file_path):
             config.read(config_file_path)
@@ -157,10 +192,14 @@ def read_config():
     log_file = config.get('DEFAULT', 'log_file', fallback=log_file)
     verbose = config.getint('DEFAULT', 'verbose', fallback=verbose)
 
-    hysteresis_temp = config.getfloat('FAN-CTRL', 'hysteresis_temp', fallback=hysteresis_temp)
-    trigger_temp = config.getfloat('FAN-CTRL', 'trigger_temp', fallback=trigger_temp)
-    max_attempts = config.getint('FAN-CTRL', 'max_attempts', fallback=max_attempts)
-    sleep_seconds = config.getfloat('FAN-CTRL', 'sleep_seconds', fallback=sleep_seconds)
+    hysteresis_temp = config.getfloat(
+        'FAN-CTRL', 'hysteresis_temp', fallback=hysteresis_temp)
+    trigger_temp = config.getfloat(
+        'FAN-CTRL', 'trigger_temp', fallback=trigger_temp)
+    max_attempts = config.getint(
+        'FAN-CTRL', 'max_attempts', fallback=max_attempts)
+    sleep_seconds = config.getfloat(
+        'FAN-CTRL', 'sleep_seconds', fallback=sleep_seconds)
 
 
 def setup_logging(log_file: str, verbose_level: int) -> logging.Logger:
@@ -177,34 +216,14 @@ def setup_logging(log_file: str, verbose_level: int) -> logging.Logger:
     # Reference: https://docs.python.org/3.9/howto/logging.html
     new_logger = logging.getLogger(MODULE_NAME)
     new_logger.addHandler(JournalHandler())
-    new_logger.setLevel(logging.DEBUG if verbose_level >=2 else logging.INFO)
+    new_logger.setLevel(logging.DEBUG if verbose_level >= 2 else logging.INFO)
     # Reference for log to file: https://stackoverflow.com/questions/6386698/how-to-write-to-a-file-using-the-logging-python-module
     fh = logging.FileHandler(log_file)
-    fh.setLevel(logging.DEBUG if verbose_level >=2 else logging.INFO)
+    fh.setLevel(logging.DEBUG if verbose_level >= 2 else logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
     new_logger.addHandler(fh)
     return new_logger
-
-
-def init_communication(logger: logging.Logger):
-    """Initialize communication with i2c device, also writing to log.
-       If communication fails, exit with error code 2.
-    """
-    logger.info("Starting {} log.".format(MODULE_NAME))
-
-    try:
-        with smbus2.SMBus(bus_number) as i2c:
-            i2c.enable_pec(True)
-            i2c.write_byte_data(DEVICE_ADDR, FAN_REGISTER_ADDR, 0x00)
-    except Exception as e:
-        msg = "Cannot open i2c device at bus {}, address '{}'! Aborting.\n".format(bus_number, hex(DEVICE_ADDR))
-        logger.critical(msg, exc_info=True)
-        exit(2)
-    else:
-        logger.info("Connected successfully to i2c device at bus {}, address '{}'.".format(bus_number, hex(DEVICE_ADDR)))
-
-    logger.info("Initial Temp: {:.2f}°C, trigger temp: >={:.2f}°C, hys. temp: {:.2f}°C ".format(get_cpu_temp(), trigger_temp, -hysteresis_temp))
 
 
 def main():
@@ -228,10 +247,10 @@ def main():
     signal.signal(signal.SIGQUIT, tidyup)
     signal.signal(signal.SIGTERM, tidyup)
 
-    #Init
+    # Init
     init_communication(logger)
 
-    #Main loop
+    # Main loop
     while True:
         fan_action = FanActions.NONE
         temperature = get_cpu_temp()
@@ -243,14 +262,16 @@ def main():
 
         if fan_action != FanActions.NONE:
             set_fan(fan_action)
-            logger.info('Temp: {:.2f}°C, Fan action: {}'.format(temperature, fan_action.name))
+            logger.info('Temp: {:.2f}°C, Fan action: {}'.format(
+                temperature, fan_action.name))
         elif verbose >= 2:
             logger.debug('Temp: {:.2f}°C'.format(temperature))
 
         sleep(sleep_seconds)
 
+
 if __name__ == "__main__":
     main()
 
-#Manual OFF: sudo i2cset -y 1 0x0d 0x08 0x00
-#Manual  ON:  sudo i2cset -y 1 0x0d 0x08 0x01
+# Manual OFF: sudo i2cset -y 1 0x0d 0x08 0x00
+# Manual  ON:  sudo i2cset -y 1 0x0d 0x08 0x01
